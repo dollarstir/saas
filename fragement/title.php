43a9e3ab67639cf2e529b232adbbde717a758672
dollarstir <kpin463@gmail.com>
2022-05-04 12:11:43 +0000

    sdasds

--- fragement/title.php
+++ fragement/title.php
@@ -2,8 +2,5 @@
 
 function title($page)
 {
-    $a = new Sel();
-    $response = $a->getall('settings');
-    $title = $response[0]['appname'];
-    echo $title.' - '.$page;
+    selectAll('appsettings');
 }
