74a2cac4108e4c80db5a138f850f41116b104bfc
dollarstir <kpin463@gmail.com>
2022-05-30 11:14:44 +0000

    footer

--- main/main1.php
+++ main/main1.php
@@ -788,23 +788,6 @@ Subscribe Now
 <i class="icofont-scroll-bubble-up"></i>
 </div>
 
-<script data-cfasync="false" src="main/../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="main/assets/js/jquery.min.js"></script>
-<script src="main/assets/js/bootstrap.bundle.min.js"></script>
-
-<script src="main/assets/js/jquery.ajaxchimp.min.js"></script>
-
-<script src="main/assets/js/form-validator.min.js"></script>
-
-<script src="main/assets/js/contact-form-script.js"></script>
-
-<script src="main/assets/js/owl.carousel.min.js"></script>
-
-<script src="main/assets/js/jquery.magnific-popup.min.js"></script>
-
-<script src="main/assets/js/wow.min.js"></script>
-
-<script src="main/assets/js/meanmenu.js"></script>
-
-<script src="main/assets/js/custom.js"></script>
+<?php involve('script'); ?>
 </body>
 </html>
