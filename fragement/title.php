b3384546d39f2e28f63a07feddc238d658767c3e
dollarstir <kpin463@gmail.com>
2022-05-30 11:24:45 +0000

    working with agents display

--- fragement/title.php
+++ fragement/title.php
@@ -22,3 +22,37 @@ function logo()
 
     return $ss;
 }
+// getting agents
+
+function agents()
+{
+    $res = fetchall('agent');
+
+    foreach ($res as $row) {
+        echo '<div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s">
+        <div class="worker-card">
+            <div class="worker-img">
+                <img loading="lazy" src="">
+            </div>
+            <div class="worker-text">
+                <h3>John Doe</h3>
+                <p>Road Freight Collector</p>
+            </div>
+            <div class="worker-social">
+                <a href="#" target="_blank">
+                    <i class="icofont-facebook"></i>
+                </a>
+                <a href="#" target="_blank">
+                    <i class="icofont-twitter"></i>
+                </a>
+                <a href="#" target="_blank">
+                    <i class="icofont-instagram"></i>
+                </a>
+                <a href="#" target="_blank">
+                    <i class="icofont-swoosh-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>';
+    }
+}
--- main/agent.php
+++ main/agent.php
@@ -2,370 +2,211 @@
 involve('header');
 topbar('Our Trusted Agents');
 ?>
+
 <body>
 
-<div class="loader-content">
-<div class="d-table">
-<div class="d-table-cell">
-<div class="sk-folding-cube">
-<div class="sk-cube1 sk-cube"></div>
-<div class="sk-cube2 sk-cube"></div>
-<div class="sk-cube4 sk-cube"></div>
-<div class="sk-cube3 sk-cube"></div>
-</div>
-</div>
-</div>
-</div>
+    <div class="loader-content">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="sk-folding-cube">
+                    <div class="sk-cube1 sk-cube"></div>
+                    <div class="sk-cube2 sk-cube"></div>
+                    <div class="sk-cube4 sk-cube"></div>
+                    <div class="sk-cube3 sk-cube"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-<div class="navbar-area">
+    <div class="navbar-area">
 
-<div class="mobile-nav">
-<a href="index.html" class="logo">
-<img loading ="lazy" src="main/assets/img/logo.png" alt="logo">
-</a>
-</div>
+        <div class="mobile-nav">
+            <a href="index.html" class="logo">
+                <img loading="lazy" src="main/assets/img/logo.png" alt="logo">
+            </a>
+        </div>
 
-<div class="main-nav">
-<div class="container">
-<nav class="navbar navbar-expand-lg navbar-light">
-<a class="navbar-brand" href="index.html">
-<img loading ="lazy" src="main/assets/img/logo.png" alt="logo">
-</a>
-<div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
-<ul class="navbar-nav ms-auto">
-<li class="nav-item">
-<a href="#" class="nav-link dropdown-toggle">Home</a>
-<ul class="dropdown-menu">
-<li class="nav-item">
-<a href="index.html" class="nav-link">Home Page 1</a>
-</li>
-<li class="nav-item">
-<a href="index-two.html" class="nav-link">Home Page 2</a>
-</li>
-<li class="nav-item">
-<a href="index-three.html" class="nav-link">Home Page 3</a>
-</li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="about.html" class="nav-link">About</a>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link dropdown-toggle">Services</a>
-<ul class="dropdown-menu">
- <li class="nav-item">
-<a href="service.html" class="nav-link">Services One</a>
-</li>
-<li class="nav-item">
-<a href="service-two.html" class="nav-link">Services Two</a>
-</li>
-<li class="nav-item">
-<a href="ocean-freight.html" class="nav-link">Ocean Freight</a>
-</li>
-<li class="nav-item">
-<a href="service-details.html" class="nav-link">Services Details</a>
-</li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link dropdown-toggle active">Workers</a>
-<ul class="dropdown-menu">
-<li class="nav-item">
-<a href="worker.html" class="nav-link">Workers Style One</a>
-</li>
-<li class="nav-item">
-<a href="worker-two.html" class="nav-link active">Workers Style Two</a>
-</li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link dropdown-toggle">Pages</a>
-<ul class="dropdown-menu">
-<li class="nav-item">
-<a href="faq.html" class="nav-link">FAQ</a>
-</li>
-<li class="nav-item">
-<a href="404.html" class="nav-link">404</a>
-</li>
-<li class="nav-item">
-<a href="why-choose.html" class="nav-link">Why Choose Us</a>
-</li>
-<li class="nav-item">
-<a href="privacy.html" class="nav-link">Privacy policy</a>
-</li>
-<li class="nav-item">
-<a href="terms.html" class="nav-link">Terms & Conditions</a>
-</li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link dropdown-toggle">Blog</a>
-<ul class="dropdown-menu">
-<li class="nav-item">
-<a href="blog.html" class="nav-link">Blog</a>
-</li>
-<li class="nav-item">
-<a href="blog-two.html" class="nav-link">Blog Style Two</a>
-</li>
-<li class="nav-item">
-<a href="blog-details.html" class="nav-link">Blog Details</a>
-</li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="contact.html" class="nav-link">Contact Us</a>
-</li>
-</ul>
-</div>
-</nav>
-</div>
-</div>
-</div>
-
-
-<div class="page-title title-bg-7">
-<div class="container">
-<div class="title-text text-center">
-<h2>Workers Style Two</h2>
-<ul>
-<li>
-<a href="index.html">Home</a>
-</li>
-<li>Workers Style Two</li>
-</ul>
-</div>
-</div>
-</div>
+        <div class="main-nav">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.html">
+                        <img loading="lazy" src="main/assets/img/logo.png" alt="logo">
+                    </a>
+                    <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle">Home</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="index.html" class="nav-link">Home Page 1</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="index-two.html" class="nav-link">Home Page 2</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="index-three.html" class="nav-link">Home Page 3</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a href="about.html" class="nav-link">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle">Services</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="service.html" class="nav-link">Services One</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="service-two.html" class="nav-link">Services Two</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="ocean-freight.html" class="nav-link">Ocean Freight</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="service-details.html" class="nav-link">Services Details</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle active">Workers</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="worker.html" class="nav-link">Workers Style One</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="worker-two.html" class="nav-link active">Workers Style Two</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle">Pages</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="faq.html" class="nav-link">FAQ</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="404.html" class="nav-link">404</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="why-choose.html" class="nav-link">Why Choose Us</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="privacy.html" class="nav-link">Privacy policy</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="terms.html" class="nav-link">Terms & Conditions</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle">Blog</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="blog.html" class="nav-link">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="blog-two.html" class="nav-link">Blog Style Two</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="blog-details.html" class="nav-link">Blog Details</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a href="contact.html" class="nav-link">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
 
 
-<section class="worker-section worker-style-two pt-100 pb-70">
-<div class="container">
-<div class="section-title text-center">
-<span>Workers</span>
-<h2>Our Valuable Workers</h2>
-<p>Found parents would couldn't said on. That, feedback there made he was may blind you simple, its yet a own blind you ago hand.</p>
-</div>
-<div class="row">
-<div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s">
-<div class="worker-card">
-<div class="worker-img">
-<img loading ="lazy" src="main/assets/img/worker/1.jpg" alt="worker image">
-</div>
-<div class="worker-text">
-<h3>John Doe</h3>
-<p>Road Freight Collector</p>
-</div>
-<div class="worker-social">
-<a href="#" target="_blank">
-<i class="icofont-facebook"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-twitter"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-instagram"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-swoosh-right"></i>
-</a>
-</div>
-</div>
-</div>
-<div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">
-<div class="worker-card">
-<div class="worker-img">
-<img loading ="lazy" src="main/assets/img/worker/2.jpg" alt="worker image">
-</div>
-<div class="worker-text">
-<h3>Naimuk Waninolin</h3>
-<p>Air Freight Collector</p>
-</div>
-<div class="worker-social">
-<a href="#" target="_blank">
-<i class="icofont-facebook"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-twitter"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-instagram"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-swoosh-right"></i>
-</a>
-</div>
-</div>
-</div>
- <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">
-<div class="worker-card">
-<div class="worker-img">
-<img loading ="lazy" src="main/assets/img/worker/3.jpg" alt="worker image">
-</div>
-<div class="worker-text">
-<h3>Mark Tyson</h3>
-<p>Ocean Freight Collector</p>
-</div>
-<div class="worker-social">
-<a href="#" target="_blank">
-<i class="icofont-facebook"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-twitter"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-instagram"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-swoosh-right"></i>
-</a>
-</div>
-</div>
-</div>
-<div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
-<div class="worker-card">
-<div class="worker-img">
-<img loading ="lazy" src="main/assets/img/worker/4.jpg" alt="worker image">
-</div>
-<div class="worker-text">
-<h3>Jackson</h3>
-<p>Road Freight Collector</p>
-</div>
-<div class="worker-social">
-<a href="#" target="_blank">
-<i class="icofont-facebook"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-twitter"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-instagram"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-swoosh-right"></i>
-</a>
-</div>
-</div>
-</div>
-<div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">
-<div class="worker-card">
-<div class="worker-img">
-<img loading ="lazy" src="https://templates.hibootstrap.com/fixa/default/assets/img/worker/5.jpg" alt="worker image">
-</div>
-<div class="worker-text">
-<h3>Cameron</h3>
-<p>Road Freight Collector</p>
-</div>
-<div class="worker-social">
-<a href="#" target="_blank">
-<i class="icofont-facebook"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-twitter"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-instagram"></i>
-</a>
-</div>
-</div>
-</div>
-<div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
- <div class="worker-card">
-<div class="worker-img">
-<img loading ="lazy" src="https://templates.hibootstrap.com/fixa/default/assets/img/worker/6.jpg" alt="worker image">
-</div>
-<div class="worker-text">
-<h3>Henry</h3>
-<p>Ocean Freight Collector</p>
-</div>
-<div class="worker-social">
-<a href="#" target="_blank">
-<i class="icofont-facebook"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-twitter"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-instagram"></i>
-</a>
-</div>
-</div>
-</div>
-<div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">
-<div class="worker-card">
-<div class="worker-img">
-<img loading ="lazy" src="https://templates.hibootstrap.com/fixa/default/assets/img/worker/7.jpg" alt="worker image">
-</div>
-<div class="worker-text">
-<h3>Dylan</h3>
-<p>Ocean Freight Collector</p>
-</div>
-<div class="worker-social">
-<a href="#" target="_blank">
-<i class="icofont-facebook"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-twitter"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-instagram"></i>
-</a>
-</div>
-</div>
-</div>
-<div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".7s">
-<div class="worker-card">
-<div class="worker-img">
-<img loading ="lazy" src="https://templates.hibootstrap.com/fixa/default/assets/img/worker/8.jpg" alt="worker image">
-</div>
-<div class="worker-text">
-<h3>Frank</h3>
-<p>Road Freight Collector</p>
-</div>
-<div class="worker-social">
-<a href="#" target="_blank">
-<i class="icofont-facebook"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-twitter"></i>
-</a>
-<a href="#" target="_blank">
-<i class="icofont-instagram"></i>
-</a>
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
+    <div class="page-title title-bg-7">
+        <div class="container">
+            <div class="title-text text-center">
+                <h2>Workers Style Two</h2>
+                <ul>
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>Workers Style Two</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 
-<div class="newsletter-section">
-<div class="container">
-<div class="newsletter-area">
-<h2>Join Newsletter For Daily Update</h2>
-<div class="col-lg-6 p-0">
-<form class="newsletter-form" data-bs-toggle="validator">
-<input type="email" class="form-control" placeholder="Enter Your Email" name="EMAIL" required autocomplete="off">
-<button class="default-btn electronics-btn" type="submit">
-Subscribe Now
-</button>
-<div id="validator-newsletter" class="form-result"></div>
-</form>
-</div>
-<img loading ="lazy" src="main/assets/img/newsletter-img.png" alt="newsletter image">
-</div>
-</div>
-</div>
+    <section class="worker-section worker-style-two pt-100 pb-70">
+        <div class="container">
+            <div class="section-title text-center">
+                <span>Workers</span>
+                <h2>Our Valuable Workers</h2>
+                <!-- <p>Found parents would couldn't said on. That, feedback there made he was may blind you simple, its yet a own blind you ago hand.</p> -->
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-duration="1s">
+                    <div class="worker-card">
+                        <div class="worker-img">
+                            <img loading="lazy" src="main/assets/img/worker/1.jpg" alt="worker image">
+                        </div>
+                        <div class="worker-text">
+                            <h3>John Doe</h3>
+                            <p>Road Freight Collector</p>
+                        </div>
+                        <div class="worker-social">
+                            <a href="#" target="_blank">
+                                <i class="icofont-facebook"></i>
+                            </a>
+                            <a href="#" target="_blank">
+                                <i class="icofont-twitter"></i>
+                            </a>
+                            <a href="#" target="_blank">
+                                <i class="icofont-instagram"></i>
+                            </a>
+                            <a href="#" target="_blank">
+                                <i class="icofont-swoosh-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
 
 
-<?php involve('footer'); ?>
+    <div class="newsletter-section">
+        <div class="container">
+            <div class="newsletter-area">
+                <h2>Join Newsletter For Daily Update</h2>
+                <div class="col-lg-6 p-0">
+                    <form class="newsletter-form" data-bs-toggle="validator">
+                        <input type="email" class="form-control" placeholder="Enter Your Email" name="EMAIL" required
+                            autocomplete="off">
+                        <button class="default-btn electronics-btn" type="submit">
+                            Subscribe Now
+                        </button>
+                        <div id="validator-newsletter" class="form-result"></div>
+                    </form>
+                </div>
+                <img loading="lazy" src="main/assets/img/newsletter-img.png" alt="newsletter image">
+            </div>
+        </div>
+    </div>
 
 
-<div class="top-btn">
-<i class="icofont-scroll-bubble-up"></i>
-</div>
+    <?php involve('footer'); ?>
 
-<?php involve('script'); ?>
+
+    <div class="top-btn">
+        <i class="icofont-scroll-bubble-up"></i>
+    </div>
+
+    <?php involve('script'); ?>
 </body>
+
 </html>
