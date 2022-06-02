d17a70583c83148aa680281066464f5b58f2964f
dollarstir <kpin463@gmail.com>
2022-06-02 12:19:00 +0000

    path

--- fragement/title.php
+++ fragement/title.php
@@ -215,8 +215,22 @@ function services()
     }
 }
 
-function correct($link)
+function correct($path = '')
 {
+    if (strpos($path, 'http') || strpos($path, 'www.')) {
+        return $path;
+    }
+
+    $root = explode('/core', __DIR__)[0];
+    $root = array_reverse(explode('/', $root))[0];
+
+    $uri = $_SERVER['REQUEST_URI'];
+
+    if (!preg_match("/^\/?$root/", $uri)) {
+        return "/$path";
+    }
+
+    return "/$root/$path";
 }
 function testimony()
 {
